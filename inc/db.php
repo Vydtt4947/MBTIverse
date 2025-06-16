cb629ea7b81f57fbae4b68565c0be8b4d6b5a334
ThuyVyDinh <thuyvydinh3131@gmail.com>
2025-06-16 20:22:34 +0700

    Update db.php

--- inc/db.php
+++ inc/db.php
@@ -1,1 +1,14 @@
+<?php
+$host = 'localhost';
+$username = 'root';
+$password = '123456'; 
+$database = 'mbti_schema'; 
+
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Kết nối thất bại: " . $conn->connect_error);
+}
+// Nếu không bị lỗi thì kết nối thành công
+?>
 
